00beb1494ea520d2d93781d5d156ab99d5c65a8c
Kirsten Sison <31249788+Skentir@users.noreply.github.com>
2019-07-15 04:08:41 +0800

    Added QSort

--- Linked-List.c
+++ Linked-List.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-    int nodeVariable;
-	struct Node* (nextNode);
-}Node;
-
-typedef struct {
-    struct Node* (nodes);
-    int linkSize;
-}LinkedList;
-    
-LinkedList createList(int nSize){
-    LinkedList linkList;
-    
-    linkList.linkSize = nSize;
-    linkList.nodes = (Node*) malloc(sizeof(Node) * nSize);
-    
-    return linkList;
-}
+#include "linked_list.h"
 int main() {
+    printf("grease is the best musical fite me\n");
+    int length = 5;
+    Node *head = NULL;
+    createList(&head, length);
+    display(head);
+    printf("\nQuicksort by swapping data: \n");
+    Node *tmp = QuickSortData(head, TailNode(head));
+    display(tmp);
+    printf("\nQuicksort by swapping pointers: \n");
     return 0;
 }
+
+/*
+ * Linked List Basic Operations: Parlante, N. (2001) Linked List Basics  Stanford.edu
+ * Swap: Flowers, B. H., Flowers, S. B. H., & Flowers, F. and F. R. B. H. (2000). An Introduction to Numerical Methods in C++. Oxford University Press.
+ * QSort: https://github.com/Aufuray/quicksort-on-linkedlist/blob/master/codefile.c
+ * */
