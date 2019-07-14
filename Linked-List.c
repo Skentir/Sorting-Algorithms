63b81f424c35e642cd83fc820a3469f056f59be3
Rafael Maderazo <48158388+RafMdrzo@users.noreply.github.com>
2019-07-14 23:57:46 +0800

    Update Linked-List.c

--- Linked-List.c
+++ Linked-List.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+typedef struct{
+    int nodeVariable;
+    Node nextNode;
+}Node;
+
+/*Node *n;
+n = (Node*) malloc(sizeof(Node) * linkSize);
+
+*/
+
 int main() {
     return 0;
 }
